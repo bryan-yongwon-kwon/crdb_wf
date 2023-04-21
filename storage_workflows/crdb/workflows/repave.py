f7bee068c5fd1705d078d39b2b144cab99663008
aoc-dd <ao.chen@doordash.com>
2023-04-20 23:46:34 -0700

    Add repave app

---
+++ storage_workflows/crdb/workflows/repave.py
@@ -0,0 +1,21 @@
+import typer
+from storage_workflows.crdb.operations.workflow_pre_run_check import WorkflowPreRunCheck
+from storage_workflows.setup_env import setup_env
+
+app = typer.Typer()
+
+@app.command()
+def repave_pre_check(deployment_env, region, cluster_name):
+    setup_env(deployment_env, region)
+    if (WorkflowPreRunCheck.backup_job_is_running(cluster_name)
+        or WorkflowPreRunCheck.restore_job_is_running(cluster_name)
+        or WorkflowPreRunCheck.schema_change_job_is_running(cluster_name)
+        or WorkflowPreRunCheck.row_level_ttl_job_is_running(cluster_name)
+        or WorkflowPreRunCheck.unhealthy_ranges_exist(cluster_name)
+        or WorkflowPreRunCheck.instances_not_in_service_exist(cluster_name)):
+        raise Exception("Pre run check failed")
+    else:
+        print("Check passed")
+
+if __name__ == "__main__":
+    app()
