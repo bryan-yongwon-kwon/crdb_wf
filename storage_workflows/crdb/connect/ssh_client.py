be6c3a68353282dca3a132b8414831fc0a5c93a0
aoc-dd <ao.chen@doordash.com>
2023-04-27 00:36:00 -0700

    Add crontab check

---
+++ storage_workflows/crdb/connect/ssh_client.py
@@ -0,0 +1,22 @@
+import os
+from functools import cached_property
+from paramiko import SSHClient,RSAKey
+
+class SSHClient:
+    
+    def __init__(self, cluster_name:str):
+        self._cluster_name = cluster_name
+
+    @cached_property
+    def ssh_private_key(self):
+        return RSAKey.from_private_key(os.getenv('DEPLOYMENT_ENV'), os.getenv('PASSPHRASE'))
+
+    @cached_property
+    def client(self):
+        return SSHClient()
+    
+    def connect_to_node(self, ip):
+        self.client.connect(ip, username='ubuntu')
+
+    def execute_command(self, command):
+        return self.client.exec_command(command)
--- storage_workflows/crdb/workflows/repave.py
+++ storage_workflows/crdb/workflows/repave.py
@@ -1,6 +1,7 @@
 import typer
 from storage_workflows.crdb.operations.workflow_pre_run_check import WorkflowPreRunCheck
 from storage_workflows.setup_env import setup_env
+from storage_workflows.crdb.connect.ssh_client import SSHClient
 
 app = typer.Typer()
 
@@ -16,6 +17,20 @@ def repave_pre_check(deployment_env, region, cluster_name):
         raise Exception("Pre run check failed")
     else:
         print("Check passed")
+
+@app.command()
+def check_crontab(deployment_env, region, cluster_name):
+    setup_env(deployment_env, region)
+    ssh_client = SSHClient()
+    ssh_client.connect_to_node("10.4.112.109")
+    stdin, stdout, stderr = ssh_client.execute_command("sudo crontab -l")
+    print("stdin")
+    print(stdin)
+    print("stdout")
+    print(stdout)
+    print("stderr")
+    print(stderr)
+
 
 if __name__ == "__main__":
     app()
