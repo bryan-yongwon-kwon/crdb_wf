941eb24f9cc9b893b7ef9b3df17c7a412d1bc255
aoc-dd <ao.chen@doordash.com>
2023-05-02 09:58:14 -0700

    Fix the list_nodes func

--- storage_workflows/crdb/api_gateway/crdb_api_gateway.py
+++ storage_workflows/crdb/api_gateway/crdb_api_gateway.py
@@ -5,16 +5,12 @@ class CrdbApiGateway:
 
     @staticmethod
     def login():
-        test_response = post("https://{}".format(CrdbApiGateway.__make_url()))
-        print("Test connect UI1: {}".format(test_response))
-        print("Test connect UI2: {}".format(test_response.content))
-        print("Test connect UI3: {}".format(test_response.text))
         response = post("https://{}/api/v2/login/?username=root&password={}".format(CrdbApiGateway.__make_url(), os.getenv('ROOT_PASSWORD')))
         return response.json()["session"]
     
     @staticmethod
     def list_nodes(session:str, limit=200, offset=0):
-        return get("{}/api/v2/nodes/?limit={}}&offset={}".format(CrdbApiGateway.__make_url(), limit, offset),
+        return get("https://{}/api/v2/nodes/?limit={}&offset={}".format(CrdbApiGateway.__make_url(), limit, offset),
                        headers={"X-Cockroach-API-Session":session}).json()
 
     @staticmethod
