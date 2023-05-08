e7335744e65aba19f0575ff8b4925cfd7e61c5cd
aoc-dd <ao.chen@doordash.com>
2023-05-08 14:51:04 -0700

    Add cronjob scheduling

--- storage_workflows/crdb/commands/cluster_check.py
+++ storage_workflows/crdb/commands/cluster_check.py
@@ -2,7 +2,6 @@ import typer
 from storage_workflows.setup_env import setup_env
 from storage_workflows.crdb.connect.ssh import SSH
 from storage_workflows.crdb.cluster.node import Node
-from storage_workflows.crdb.aws.auto_scaling_group import AutoScalingGroup
 
 app = typer.Typer()
 
@@ -14,20 +13,21 @@ def check_crontab(deployment_env, region, cluster_name):
     nodes = Node.get_nodes()
     nodes.sort(key=lambda node: node.started_at, reverse=True)
     node_ip_list = list(map(lambda node: node.ip_address, nodes))
-    print("New node IP: {}".format(nodes[0].ip_address))
     new_node_ssh_client = SSH(nodes[0].ip_address)
     new_node_ssh_client.connect_to_node()
     for ip in node_ip_list:
         ssh_client = SSH(ip)
-        print("ip: {}".format(ip))
+        print("Listing cron jobs for: {}".format(ip))
         ssh_client.connect_to_node()
         stdin, stdout, stderr = ssh_client.execute_command("sudo crontab -l")
+        lines = stdout.readlines()
         errors = stderr.readlines()
-        print("stdout: {}".format(stdout.readlines()))
+        print("stdout: {}".format(lines))
         print("stderr: {}".format(errors))
         if errors:
             continue
         copy_cron_scripts_to_new_node(ssh_client, new_node_ssh_client)
+        schedule_cron_jobs(lines, new_node_ssh_client)
 
 
 def copy_cron_scripts_to_new_node(old_node_ssh_client: SSH, new_node_ssh_client: SSH):
@@ -38,6 +38,15 @@ def copy_cron_scripts_to_new_node(old_node_ssh_client: SSH, new_node_ssh_client:
         file_path = CRONTAB_SCRIPTS_DIR + script_name
         lines = old_node_ssh_client.read_remote_file_with_root(file_path)
         new_node_ssh_client.write_remote_file_with_root(file_lines=lines, file_path=file_path)
+
+def schedule_cron_jobs(crontab_file_lines:list, new_node_ssh_client:SSH):
+    new_node_ssh_client.execute_command('sudo mkdir /var/log/crdb/export_logs && \
+                                        ln -s /var/log/crdb/export_logs /root/export_logs && \
+                                        ln -s /var/log/crdb/export_logs /root/.cockroach-certs/export_logs')
+    for line in crontab_file_lines:
+        if line[0] == '#':
+            continue
+        new_node_ssh_client.execute_command('(crontab -l 2>/dev/null; {}") | crontab -'.format(line))
 
 
 
