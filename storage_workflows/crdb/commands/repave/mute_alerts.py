a96d508ad014112eb133bb5360bceb894e51ce0d
meha-kaushik <meha.kaushik@doordash.com>
2023-05-15 14:24:28 -0700

    mute alerts separately

--- storage_workflows/crdb/commands/repave/mute_alerts.py
+++ storage_workflows/crdb/commands/repave/mute_alerts.py
@@ -5,37 +5,44 @@ app = typer.Typer()
 
 @app.command()
 def mute_alerts_repave(cluster_name):    
-    label_matchers = [
-        {
+    cluster_name_label_matcher = {
             "name": "cluster",
             "type": "EXACT",
             "value": cluster_name
-        },
-        {
+        }
+    
+    live_node_count_changed_label_matcher = {
             "name": "Description",
             "type": "EXACT",
             "value": "The count of live nodes has decreased"
-        },
-        {
+        }
+
+    changefeed_stoppped_label_matcher = {
             "name": "Description",
             "type": "EXACT",
             "value": "Changefeed is Stopped"
-        },
-        {
+        }
+    
+    underreplicated_range_label_matcher = {
             "name": "Description",
             "type": "EXACT",
             "value": "Underreplicated Range Detected"
-        },
-        {
-            "name": "Description",
-            "type": "EXACT",
-            "value": "Incremental or full backup failed."
         }
-    ]
     
+    backup_failed_label_matcher = {
+            "name": "cluster",
+            "type": "EXACT",
+            "value": cluster_name
+        }
+    
+    mute_alerts_using_label_matchers([cluster_name_label_matcher, live_node_count_changed_label_matcher])
+    mute_alerts_using_label_matchers([cluster_name_label_matcher, changefeed_stoppped_label_matcher])
+    mute_alerts_using_label_matchers([cluster_name_label_matcher, underreplicated_range_label_matcher])
+    mute_alerts_using_label_matchers([cluster_name_label_matcher, backup_failed_label_matcher])
+
+def mute_alerts_using_label_matchers(label_matchers):
     client = ChronosphereApiGateway()
     client.create_muting_rule(label_matchers)
-
 
 if __name__ == "__main__":
     app()
