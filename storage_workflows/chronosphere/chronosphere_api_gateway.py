ae2db8943117531b3e686be78acdba3be9c01304
meha-kaushik <meha.kaushik@doordash.com>
2023-05-15 15:06:55 -0700

    update

--- storage_workflows/chronosphere/chronosphere_api_gateway.py
+++ storage_workflows/chronosphere/chronosphere_api_gateway.py
@@ -6,7 +6,7 @@ import os
 class ChronosphereApiGateway():
 
     @staticmethod
-    def create_muting_rule(self, label_matchers, name="Muting rule created from operator service.", starts_at=datetime.datetime.now(datetime.timezone.utc), path="/api/v1/config/muting-rules", http_method="POST"):
+    def create_muting_rule(label_matchers, name="Muting rule created from operator service.", starts_at=datetime.datetime.now(datetime.timezone.utc), path="/api/v1/config/muting-rules", http_method="POST"):
         #rules expire in 1 hour
         ends_at = starts_at + datetime.timedelta(hours=1)
         chronosphere_api_token = os.getenv('CHRONOSPHERE_API_TOKEN')
