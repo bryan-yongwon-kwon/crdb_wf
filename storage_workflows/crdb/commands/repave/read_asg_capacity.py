803c91205bff29e7c5facb9c73daaad8673ee676
meha-kaushik <meha.kaushik@doordash.com>
2023-06-04 12:18:34 -0700

    update

--- storage_workflows/crdb/commands/repave/read_asg_capacity.py
+++ storage_workflows/crdb/commands/repave/read_asg_capacity.py
@@ -2,16 +2,17 @@ import typer
 from storage_workflows.crdb.api_gateway.auto_scaling_group_gateway import AutoScalingGroupGateway
 from storage_workflows.crdb.connect.crdb_connection import CrdbConnection
 from storage_workflows.crdb.aws.auto_scaling_group import AutoScalingGroup
+from storage_workflows.setup_env import setup_env
 
 app = typer.Typer()
 
 @app.command()
-def read_asg_capacity(cluster_name):
+def read_asg_capacity(cluster_name, deployment_env, region):
+    setup_env(deployment_env, region, cluster_name)
     asg_name = AutoScalingGroup.find_auto_scaling_group_by_cluster_name(cluster_name)
     capacity = AutoScalingGroupGateway.get_auto_scaling_group_capacity(asg_name)
     print("Read capacity:" + capacity)
-    insert_into_cluster_info(cluster_name, capacity)
-    return{} 
+    return insert_into_cluster_info(cluster_name, capacity)
 
 def insert_into_cluster_info(cluster_name, node_count):
     connection = CrdbConnection.get_crdb_connection(cluster_name)
