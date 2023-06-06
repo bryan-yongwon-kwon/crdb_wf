18238f8859580a5423d14f4a0855f7270636eef5
aoc-dd <ao.chen@doordash.com>
2023-06-06 16:32:08 -0700

    Refactor the mute_alerts_repave func

--- storage_workflows/crdb/commands/repave.py
+++ storage_workflows/crdb/commands/repave.py
@@ -44,35 +44,13 @@ def refresh_etl_load_balancer(deployment_env, region, cluster_name):
 
 @app.command()
 def mute_alerts_repave(cluster_name):    
-    cluster_name_label_matcher = {
-            "name": "cluster",
-            "type": "EXACT",
-            "value": cluster_name
-        }
-    
-    live_node_count_changed_label_matcher = {
-            "name": "Description",
-            "type": "EXACT",
-            "value": "The count of live nodes has decreased"
-        }
-
-    changefeed_stoppped_label_matcher = {
-            "name": "Description",
-            "type": "EXACT",
-            "value": "Changefeed is Stopped"
-        }
-    
-    underreplicated_range_label_matcher = {
-            "name": "Description",
-            "type": "EXACT",
-            "value": "Underreplicated Range Detected"
-        }
-    
-    backup_failed_label_matcher = {
-            "name": "Description",
-            "type": "EXACT",
-            "value": "Incremental or full backup failed."
-        }
+    def make_alert_label_matcher(name, type, value):
+        return {"name": name, "type": type, "value": value}
+    cluster_name_label_matcher = make_alert_label_matcher("cluster", "EXACT", cluster_name)
+    live_node_count_changed_label_matcher = make_alert_label_matcher("Description", "EXACT", "The count of live nodes has decreased")
+    changefeed_stoppped_label_matcher = make_alert_label_matcher("Description", "EXACT", "Changefeed is Stopped")    
+    underreplicated_range_label_matcher = make_alert_label_matcher("Description", "EXACT", "Underreplicated Range Detected")
+    backup_failed_label_matcher = make_alert_label_matcher("Description", "EXACT", "Incremental or full backup failed.")
     
     ChronosphereApiGateway.create_muting_rule([cluster_name_label_matcher, live_node_count_changed_label_matcher])
     ChronosphereApiGateway.create_muting_rule([cluster_name_label_matcher, changefeed_stoppped_label_matcher])
