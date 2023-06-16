ec13a1b08411514c313eb75e6d74b14ebe42db63
meha-kaushik <meha.kaushik@doordash.com>
2023-06-15 19:21:29 -0700

    move asg functions a new file for testing

--- storage_workflows/crdb/commands/repave.py
+++ storage_workflows/crdb/commands/repave.py
@@ -59,23 +59,5 @@ def mute_alerts_repave(cluster_name):
     ChronosphereApiGateway.create_muting_rule([cluster_name_label_matcher, underreplicated_range_label_matcher])
     ChronosphereApiGateway.create_muting_rule([cluster_name_label_matcher, backup_failed_label_matcher])
 
-@app.command
-def read_and_increase_asg_capacity(cluster_name, deployment_env, region):
-    setup_env(deployment_env, region, cluster_name)
-    asg = AutoScalingGroup.find_auto_scaling_group_by_cluster_name(cluster_name)
-    capacity = asg.capacity
-    print("ASG capacity: " + str(capacity))
-    instances=[]
-    for instance in asg.instances:
-        instances.append(instance.instance_id)
-    MetadataDBOperations.persist_asg_old_instance_ids(cluster_name, instances)
-    delete_old_nodes_from_asg(asg.name, cluster_name)
-    #AutoScalingGroupGateway.update_auto_scaling_group_capacity(asg.name, 2*capacity)
-
-@app.command
-def delete_old_nodes_from_asg(asg_name, cluster_name):
-    old_instances = MetadataDBOperations.get_old_nodes(cluster_name)
-    AutoScalingGroupGateway.remove_instance_from_autoscaling_group(old_instances[0], asg_name)
-
 if __name__ == "__main__":
     app()
--- storage_workflows/metadata_db/metadata_db_operations.py
+++ storage_workflows/metadata_db/metadata_db_operations.py
@@ -7,7 +7,7 @@ class MetadataDBOperations:
     @staticmethod
     def metadata_db_connection():
         connection = psycopg2.connect(
-            database="storage-metadata_prod",
+            database="crdb_workflows",
             port="26257",
             user="storage_metadata_app_20230509",
             host="storage-metadata-crdb.us-west-2.aws.ddnw.net",
@@ -34,27 +34,19 @@ class MetadataDBOperations:
             return cursor.fetchone()
 
     @staticmethod
-    def use_db(connection,database_name):
-        sql_query = "use '{}';".format(database_name)
-        MetadataDBOperations.execute_sql(connection, sql_query, need_commit=True)
-        return
-
-    @staticmethod
     def persist_asg_old_instance_ids(cluster_name, instances):
         connection = MetadataDBOperations.metadata_db_connection(True)
-        MetadataDBOperations.use_db(connection, "crdb_workflows")
         node_list = ','.join(instances)
         node_list = str('{') + node_list + str('}')
-        sql_query = "INSERT INTO clusters_info (cluster_name, node_list) VALUES ('{}', '{}') ON CONFLICT (cluster_name) " \
-                    "DO UPDATE SET (node_list) = ('{}');".format(cluster_name, node_list, node_list)
+        sql_query = "INSERT INTO clusters_info (cluster_name, instance_ids) VALUES ('{}', '{}') ON CONFLICT (cluster_name) " \
+                    "DO UPDATE SET (instance_ids) = ('{}');".format(cluster_name, node_list, node_list)
         MetadataDBOperations.execute_sql(connection, sql_query, need_commit=True)
         return
 
     @staticmethod
     def get_old_nodes(cluster_name):
         connection = MetadataDBOperations.metadata_db_connection(True)
-        MetadataDBOperations.use_db(connection, "crdb_workflows")
-        sql_query = "SELECT node_list FROM clusters_info WHERE cluster_name ='{}';".format(cluster_name)
+        sql_query = "SELECT instance_ids FROM clusters_info WHERE cluster_name ='{}';".format(cluster_name)
         instances = MetadataDBOperations.execute_sql(connection, sql_query, need_fetchone=True)
         print("List of old instances: " + str(instances))
         return instances
