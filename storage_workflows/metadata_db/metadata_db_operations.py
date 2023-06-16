123f39b91d7ceedb2c7f2ecf2ca75e7388ca6b11
meha-kaushik <meha.kaushik@doordash.com>
2023-06-15 22:50:23 -0700

    fix

--- storage_workflows/metadata_db/metadata_db_operations.py
+++ storage_workflows/metadata_db/metadata_db_operations.py
@@ -35,7 +35,7 @@ class MetadataDBOperations:
 
     @staticmethod
     def persist_asg_old_instance_ids(cluster_name, instances):
-        connection = MetadataDBOperations.metadata_db_connection(True)
+        connection = MetadataDBOperations.metadata_db_connection()
         node_list = ','.join(instances)
         node_list = str('{') + node_list + str('}')
         sql_query = "INSERT INTO clusters_info (cluster_name, instance_ids) VALUES ('{}', '{}') ON CONFLICT (cluster_name) " \
@@ -45,7 +45,7 @@ class MetadataDBOperations:
 
     @staticmethod
     def get_old_nodes(cluster_name):
-        connection = MetadataDBOperations.metadata_db_connection(True)
+        connection = MetadataDBOperations.metadata_db_connection()
         sql_query = "SELECT instance_ids FROM clusters_info WHERE cluster_name ='{}';".format(cluster_name)
         instances = MetadataDBOperations.execute_sql(connection, sql_query, need_fetchone=True)
         print("List of old instances: " + str(instances))
