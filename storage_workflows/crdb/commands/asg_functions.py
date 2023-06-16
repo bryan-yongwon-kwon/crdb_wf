b9def8db11444552d6e08baed730637c3f001ec3
meha-kaushik <meha.kaushik@doordash.com>
2023-06-15 20:18:31 -0700

    nit

--- storage_workflows/crdb/commands/asg_functions.py
+++ storage_workflows/crdb/commands/asg_functions.py
@@ -20,7 +20,6 @@ def read_and_increase_asg_capacity(cluster_name, deployment_env, region):
     #AutoScalingGroupGateway.update_auto_scaling_group_capacity(asg.name, 2*capacity)
     return
 
-@app.command()
 def delete_old_nodes_from_asg(asg_name, cluster_name):
     old_instances = MetadataDBOperations.get_old_nodes(cluster_name)
     AutoScalingGroupGateway.remove_instance_from_autoscaling_group(old_instances[0], asg_name)
