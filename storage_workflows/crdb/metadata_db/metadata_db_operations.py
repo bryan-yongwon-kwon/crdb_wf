b6a658a1a3c1917506dedd009be01e07643e3737
meha-kaushik <meha.kaushik@doordash.com>
2023-07-06 14:09:08 -0700

    creating single connection

--- storage_workflows/crdb/metadata_db/metadata_db_operations.py
+++ storage_workflows/crdb/metadata_db/metadata_db_operations.py
@@ -8,21 +8,23 @@ logger = Logger()
 
 class MetadataDBOperations:
 
+    def __int__(self):
+        self._connection = MetadataDBConnection()
+
     def persist_asg_old_instance_ids(cluster_name, instances):
-        metadata_db_connection = MetadataDBConnection()
-        metadata_db_connection.connect()
+        self._connection.connect()
         node_list = ','.join(instances)
         node_list = str('{') + node_list + str('}')
         sql_query = "INSERT INTO clusters_info (cluster_name, instance_ids) VALUES ('{}', '{}') ON CONFLICT (cluster_name) " \
                     "DO UPDATE SET (instance_ids) = ('{}');".format(cluster_name, node_list, node_list)
-        metadata_db_connection.execute_sql(sql_query, need_commit=True)
-        metadata_db_connection.close()
+        self._connection.execute_sql(sql_query, need_commit=True)
+        self._connection.close()
         return
 
     def get_old_nodes(cluster_name):
-        metadata_db_connection = MetadataDBConnection()
-        metadata_db_connection.connect()
+        self._connection.connect()
         sql_query = "SELECT instance_ids FROM clusters_info WHERE cluster_name ='{}';".format(cluster_name)
         instances = connection.execute_sql(sql_query, need_fetchone=True)
+        self._connection.close()
         logger.info("List of old instances: " + str(instances))
         return instances
