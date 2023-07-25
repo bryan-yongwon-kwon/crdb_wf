6ee293ff14001fc1bd16ae3ca959a1ed055920e0
aoc-dd <ao.chen@doordash.com>
2023-07-25 00:52:54 -0700

    Fix the metadata operation

--- storage_workflows/crdb/commands/repave.py
+++ storage_workflows/crdb/commands/repave.py
@@ -91,7 +91,7 @@ def delete_mute_alerts(slugs:str):
 def copy_crontab(deployment_env, region, cluster_name):
     setup_env(deployment_env, region, cluster_name)
     metadata_db_operations = MetadataDBOperations()
-    instance_ids = metadata_db_operations.get_old_nodes(cluster_name, deployment_env)
+    instance_ids = metadata_db_operations.get_old_instance_ids(cluster_name, deployment_env)
     old_instance_ips = set(map(lambda instance_id: Ec2Instance.find_ec2_instance(instance_id).private_ip_address, instance_ids))
     nodes = Node.get_nodes()
     new_node = list(filter(lambda node: node.ip_address not in old_instance_ips, nodes))[0]
