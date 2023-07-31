1e9833df3906ae9f920caed121f1dca8ae9639e8
Ao Chen <102404925+aoc-dd@users.noreply.github.com>
2023-07-31 16:14:26 -0700

    Update repave.py

--- storage_workflows/crdb/commands/repave.py
+++ storage_workflows/crdb/commands/repave.py
@@ -349,7 +349,7 @@ def persist_instance_ids(deployment_env, region, cluster_name):
 @app.command()
 def send_workflow_failure_notification(deployment_env, region, cluster_name, namespace, workflow_name):
     webhook_url = os.getenv('SLACK_WEBHOOK_STORAGE_ALERTS_CRDB') if deployment_env == 'prod' else os.getenv('SLACK_WEBHOOK_STORAGE_ALERTS_CRDB_STAGING')
-    notification = SlackNotification(os.getenv('SLACK_WEBHOOK_STORAGE_ALERT_TEST'))
+    notification = SlackNotification(webhook_url)
     notification.send_notification(ContentTemplate.get_workflow_failure_content(namespace, 
                                                                                 workflow_name,
                                                                                 cluster_name,
