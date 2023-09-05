0eb105273577231d619cd0c6e70171fb02f0bb95
Bryan Kwon <bryan@cockroachlabs.com>
2023-09-05 12:33:24 -0500

    test saving hc status

--- storage_workflows/metadata_db/storage_metadata/storage_metadata.py
+++ storage_workflows/metadata_db/storage_metadata/storage_metadata.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from storage_workflows.metadata_db.storage_metadata.hc_transactions import add_cluster_health_check_txn
 from storage_workflows.metadata_db.metadata_db_connection import MetadataDBConnection
 from sqlalchemy_cockroachdb import run_transaction
+from storage_workflows.metadata_db.storage_metadata.tables.cluster_health_check import ClusterHealthCheck
+
 
 
 class StorageMetadata:
@@ -19,8 +21,14 @@ class StorageMetadata:
         self.max_records = max_records
         self.session_factory = sessionmaker(bind=self.engine)
 
-    def insert_health_check(self):
-        def callback(**kwargs):
-            add_cluster_health_check_txn(self.session_factory, **kwargs)
-
-        return run_transaction(self.session_factory, callback)
+    def insert_health_check(self, **kwargs):
+        cluster_health_check = ClusterHealthCheck(**kwargs)
+        return run_transaction(self.session_factory,
+                               lambda session: add_cluster_health_check_txn(session, cluster_name=cluster_health_check.cluster_name,
+                                                                            deployment_env=cluster_health_check.deployment_env,
+                                                                            region=cluster_health_check.region,
+                                                                            aws_account_name=cluster_health_check.aws_account_name,
+                                                                            check_type=cluster_health_check.check_type,
+                                                                            workflow_id=cluster_health_check.workflow_id,
+                                                                            check_result=cluster_health_check.check_result,
+                                                                            check_output=cluster_health_check.check_output))
