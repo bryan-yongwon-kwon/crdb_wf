e0f77661e23cb16e6c14792041bd25ff654b1c36
meha-kaushik <meha.kaushik@doordash.com>
2023-09-24 16:28:50 -0700

    test changes

--- storage_workflows/crdb/commands/user_management.py
+++ storage_workflows/crdb/commands/user_management.py
@@ -14,32 +14,32 @@ logger = Logger()
 def create_users_from_s3_objects(deployment_env, region, bucket_name, aws_account):
     setup_env(deployment_env, region, "")
     storage_metadata = StorageMetadata()
-    objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name)
+    #objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name)
     #Process the objects in the current page
-    for obj in objects:
-        content = obj['Key']
-        user_type, db_name, user_name = content.split(':')
-        logger.info("Read user_type : {0}, db_name : {1}, user_name : {2}", user_type, db_name, user_name)
-        os.environ['CLUSTER_NAME'] = db_name
-        # check if the user already exists, if yes, then do not do this.
-        storage_metadata.insert_user(cluster_name=db_name, deployment_env=deployment_env, region=region,
-                                     aws_account=aws_account, database_name=db_name, role_name=user_name,
-                                     certificate_path="cert_path")
-        sql_user = SqlUser(user_name, cluster_name=db_name)
-        sql_user.create_user()
-        logger.info("Successfully created user_name: {0}",user_name)
+    #for obj in objects:
+    #    content = obj['Key']
+    user_type, db_name, user_name = ["sql", "crdb_benchmark", "meha.kaushik"]#content.split(':')
+    logger.info("Read user_type : {0}, db_name : {1}, user_name : {2}", user_type, db_name, user_name)
+    os.environ['CLUSTER_NAME'] = db_name
+    # check if the user already exists, if yes, then do not do this.
+    storage_metadata.insert_user(cluster_name=db_name, deployment_env=deployment_env, region=region,
+                                 aws_account=aws_account, database_name=db_name, role_name=user_name,
+                                 certificate_path="cert_path")
+    sql_user = SqlUser(user_name, cluster_name=db_name)
+    sql_user.create_user()
+    logger.info("Successfully created user_name: {0}", user_name)
 
-    while next_page_token:
-        objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name, page_token=next_page_token)
-        for obj in objects:
-            content = obj['Key']
-            user_type, db_name, user_name = content.split(':')
-            logger.info("Read user_type : {0}, db_name : {1}, user_name : {2}", user_type, db_name, user_name)
-            os.environ['CLUSTER_NAME'] = db_name
-            # store in metadata db
-            storage_metadata.insert_user(cluster_name=db_name, deployment_env=deployment_env, region=region,
-                                            aws_account=aws_account, database_name=db_name, role_name=user_name,
-                                            certificate_path="cert_path")
-            sql_user = SqlUser(user_name, cluster_name=db_name)
-            sql_user.create_user()
-            logger.info("Successfully created user_name: {0}", user_name)
+    # while next_page_token:
+    #     objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name, page_token=next_page_token)
+    #     for obj in objects:
+    #         content = obj['Key']
+    #         user_type, db_name, user_name = content.split(':')
+    #         logger.info("Read user_type : {0}, db_name : {1}, user_name : {2}", user_type, db_name, user_name)
+    #         os.environ['CLUSTER_NAME'] = db_name
+    #         # store in metadata db
+    #         storage_metadata.insert_user(cluster_name=db_name, deployment_env=deployment_env, region=region,
+    #                                         aws_account=aws_account, database_name=db_name, role_name=user_name,
+    #                                         certificate_path="cert_path")
+    #         sql_user = SqlUser(user_name, cluster_name=db_name)
+    #         sql_user.create_user()
+    #         logger.info("Successfully created user_name: {0}", user_name)
