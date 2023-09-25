daaf1e8d7b27d41873241aeccdf78901c7d6cea8
Bryan Kwon <bryan@cockroachlabs.com>
2023-09-25 12:31:16 -0500

    handle root volume

--- storage_workflows/crdb/api_gateway/ebs_gateway.py
+++ storage_workflows/crdb/api_gateway/ebs_gateway.py
@@ -7,24 +7,21 @@ class EBSGateway:
     def get_ebs_volumes_for_instance(instance_id):
         ec2 = AwsSessionFactory.ec2()
 
-        # Describe volumes attached to the instance. We'll exclude 8GB root volumes.
-        volumes = ec2.describe_volumes(
+        # Describe volumes attached to the instance.
+        volumes_response = ec2.describe_volumes(
             Filters=[
                 {
                     'Name': 'attachment.instance-id',
                     'Values': [instance_id]
                 },
                 {
-                    'Name': 'size',
-                    'Values': ['8'],
-                    'Comparison': 'NotEquals'
-                },
-                {
                     'Name': 'volume-type',
                     'Values': ['gp2', 'io1', 'io2', 'st1', 'sc1']
                 }
             ]
-        )['Volumes']
+        )
+
+        volumes = [volume for volume in volumes_response['Volumes'] if volume['Size'] != 8]
 
         ebs_objects = []
         for volume in volumes:
