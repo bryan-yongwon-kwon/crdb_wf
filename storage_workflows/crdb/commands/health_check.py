8f0640404c3ed0e4013962fb2fe98ba76871d6c9
Bryan Kwon <bryan@cockroachlabs.com>
2023-09-29 11:06:48 -0500

    truncate elb name

--- storage_workflows/crdb/commands/health_check.py
+++ storage_workflows/crdb/commands/health_check.py
@@ -315,7 +315,11 @@ def etl_health_check(deployment_env, region, cluster_name):
                 elb_load_balancer.register_instances(new_instances)
                 # unhealthy_instances = []
                 logger.info(f"elb_load_balancer: {elb_load_balancer}")
-                unhealthy_instances = ElasticLoadBalancerGateway.get_out_of_service_instances(str(elb_load_balancer))
+                # account for botocore.exceptions.ClientError: An error occurred (ValidationError) when calling the
+                # DescribeInstanceHealth operation: LoadBalancer name cannot be longer than 32 characters
+                truncated_elb_load_balancer_name = str(elb_load_balancer)[:32]
+                logger.info(f"truncated_elb_load_balancer_name: {truncated_elb_load_balancer_name}")
+                unhealthy_instances = ElasticLoadBalancerGateway.get_out_of_service_instances(truncated_elb_load_balancer_name)
                 if not unhealthy_instances:
                     logger.info(f"{cluster_name}: ETL load balancer refresh completed!")
                     check_result = "pass"
