3d49a4c3f17ce47458a3318c2978baed7c678201
meha-kaushik <111391734+meha-kaushik@users.noreply.github.com>
2023-10-05 15:43:36 -0700

    repace db with cluster everywhere

--- storage_workflows/crdb/commands/user_management.py
+++ storage_workflows/crdb/commands/user_management.py
@@ -18,7 +18,7 @@ def create_users_from_s3_objects(deployment_env, region, bucket_name, aws_accoun
     for obj in objects:
         content = S3Gateway.read_object_contents(bucket_name=bucket_name, key=obj['Key'])
         user_type, cluster_name, user_name = content.split(':')
-        logger.info("Read user_type : {}, db_name : {}, user_name : {}".format(user_type, db_name, user_name))
+        logger.info("Read user_type : {}, cluster_name : {}, user_name : {}".format(user_type, cluster_name, user_name))
         os.environ['CLUSTER_NAME'] = cluster_name
         create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, cluster_name, user_name)
 
@@ -26,21 +26,21 @@ def create_users_from_s3_objects(deployment_env, region, bucket_name, aws_accoun
         objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name, page_token=next_page_token)
         for obj in objects:
             content = S3Gateway.read_object_contents(bucket_name=bucket_name, key=obj['Key'])
-            user_type, db_name, user_name = content.split(':')
-            cluster_name = db_name
-            logger.info("Read user_type : {0}, db_name : {1}, user_name : {2}", user_type, db_name, user_name)
+            user_type, cluster_name, user_name = content.split(':')
+            cluster_name = cluster_name
+            logger.info("Read user_type : {0}, cluster_name : {1}, user_name : {2}", user_type, cluster_name, user_name)
             os.environ['CLUSTER_NAME'] = cluster_name
             create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, cluster_name, user_name)
 
 
-def create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, db_name, role_name):
+def create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, cluster_name, role_name):
     storage_metadata = StorageMetadata()
-    existing_user = storage_metadata.get_user(cluster_name, region, aws_account, db_name, role_name, deployment_env)
+    existing_user = storage_metadata.get_user(cluster_name, region, aws_account, cluster_name, role_name, deployment_env)
     if existing_user is None:
-        read_only_user = ReadOnlyUser(user_name, cluster_name=db_name)
+        read_only_user = ReadOnlyUser(user_name, cluster_name=cluster_name)
         read_only_user.create_user()
         storage_metadata.insert_user(cluster_name=cluster_name, deployment_env=deployment_env, region=region,
-                                     aws_account=aws_account, database_name=db_name, role_name=role_name,
+                                     aws_account=aws_account, database_name=cluster_name, role_name=role_name,
                                      certificate_path="cert_path")
         logger.info("Successfully created user_name: {0}", user_name)
     else:
