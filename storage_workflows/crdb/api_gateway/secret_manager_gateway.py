8543410291e50810de9b2b1862fb7b95fad71ffc
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-06 17:12:28 -0500

    revise with exception handling

--- storage_workflows/crdb/api_gateway/secret_manager_gateway.py
+++ storage_workflows/crdb/api_gateway/secret_manager_gateway.py
@@ -1,24 +1,71 @@
 from storage_workflows.crdb.factory.aws_session_factory import AwsSessionFactory
-class SecretManagerGateway:
+import botocore.exceptions
 
+
+class SecretManagerGateway:
     PAGINATOR_MAX_RESULT_PER_PAGE = 100
 
     @staticmethod
-    def list_secrets(filters=[], next_token=''):
-        secret_manager_aws_client = AwsSessionFactory.secret_manager()
-        if not next_token:
-            response = secret_manager_aws_client.list_secrets(Filters=filters, MaxResults=SecretManagerGateway.PAGINATOR_MAX_RESULT_PER_PAGE, IncludePlannedDeletion=False)
-        else:
-            response = secret_manager_aws_client.list_secrets(Filters=filters, 
-                                                MaxResults=SecretManagerGateway.PAGINATOR_MAX_RESULT_PER_PAGE,
-                                                IncludePlannedDeletion=False,
-                                                NextToken=next_token)
+    def _get_secret_manager_client():
+        """Retrieve the AWS Secrets Manager client."""
+        return AwsSessionFactory.secret_manager()
+
+    @staticmethod
+    def list_secrets(filters=None, next_token=None):
+        """
+        List secrets based on filters.
+
+        Args:
+        - filters (list): List of filter conditions.
+        - next_token (str): Token for pagination.
+
+        Returns:
+        - list: List of secrets.
+        """
+        if filters is None:
+            filters = []
+
+        if len(filters) > 10:
+            raise ValueError("Number of filters exceeds the allowed limit of 10.")
+
+        secret_manager_aws_client = SecretManagerGateway._get_secret_manager_client()
+
+        try:
+            response = secret_manager_aws_client.list_secrets(
+                Filters=filters,
+                MaxResults=SecretManagerGateway.PAGINATOR_MAX_RESULT_PER_PAGE,
+                IncludePlannedDeletion=False,
+                NextToken=next_token
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ValidationException':
+                raise ValueError(f"Validation error with filters: {filters}") from e
+            raise
+
+        secrets = response.get('SecretList', [])
+
+        # Check for more secrets (pagination)
         if 'NextToken' in response:
-            response['SecretList'].extend(
-                SecretManagerGateway.list_secrets(filters, response['NextToken']))
-        return response['SecretList']
-    
+            secrets.extend(
+                SecretManagerGateway.list_secrets(filters, response['NextToken'])
+            )
+
+        return secrets
+
     @staticmethod
     def find_secret(secret_arn):
-        secret_manager_aws_client = AwsSessionFactory.secret_manager()
-        return secret_manager_aws_client.get_secret_value(SecretId=secret_arn)
+        """Retrieve the value of a specific secret based on its ARN.
+
+        Args:
+        - secret_arn (str): ARN of the secret.
+
+        Returns:
+        - dict: Secret value data.
+        """
+        secret_manager_aws_client = SecretManagerGateway._get_secret_manager_client()
+
+        try:
+            return secret_manager_aws_client.get_secret_value(SecretId=secret_arn)
+        except botocore.exceptions.ClientError as e:
+            # Handle specific errors or re-raise the exception
+            raise
