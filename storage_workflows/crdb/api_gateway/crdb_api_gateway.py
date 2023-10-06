1f0e282c8ec89c2f98ad6a73229eb614541197de
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-06 13:49:11 -0500

    remove debug logging

--- storage_workflows/crdb/api_gateway/crdb_api_gateway.py
+++ storage_workflows/crdb/api_gateway/crdb_api_gateway.py
@@ -20,13 +20,13 @@ class CrdbApiGateway:
             # Check for 401 Unauthorized
             if response.status_code == 401:
                 logger.error(f"Login failed with 401 Unauthorized. Message: {response.text}")
-                logger.error(f"url: {url}")
+                # logger.error(f"url: {url}")
                 return None
 
             # Check if the response is not 200 OK
             elif response.status_code != 200:
                 logger.error(f"Login failed with status code {response.status_code}: {response.text}")
-                logger.error(f"url: {url}")
+                # logger.error(f"url: {url}")
                 return None
 
             session = response.json().get("session")
@@ -45,7 +45,7 @@ class CrdbApiGateway:
         response = get("https://{}/api/v2/nodes/?limit={}&offset={}".format(CrdbApiGateway.__make_url(), limit, offset),
             headers={"X-Cockroach-API-Session": session}).json()
         # DEBUG
-        logger.info(f"response for CrdbApiGateway.list_nodes: {response}")
+        # logger.info(f"response for CrdbApiGateway.list_nodes: {response}")
         return response
     
     @staticmethod
@@ -55,7 +55,7 @@ class CrdbApiGateway:
         response = get("https://{}/_status/nodes/{}".format(CrdbApiGateway.__make_url(), node_id),
                    cookies=jar).json()
         # DEBUG
-        logger.info(f"response for CrdbApiGateway.get_node_details_from_endpoint: {response}")
+        # logger.info(f"response for CrdbApiGateway.get_node_details_from_endpoint: {response}")
         return response
 
     @staticmethod
@@ -64,7 +64,7 @@ class CrdbApiGateway:
         staging_url = "{}-crdb-admin.doorcrawl-int.com".format(cluster_name)
         prod_url = "{}-crdb-admin.doordash-int.com".format(cluster_name)
         # DEBUG
-        logger.info(f"response for CrdbApiGateway.__make_url: {prod_url}")
+        # logger.info(f"response for CrdbApiGateway.__make_url: {prod_url}")
         if os.getenv('DEPLOYMENT_ENV') == "staging":
             return staging_url
         return prod_url
