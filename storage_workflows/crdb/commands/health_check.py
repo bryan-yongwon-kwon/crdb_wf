45325f611ea7d693ba82482ae0bee6b559e15432
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-06 14:39:43 -0500

    print secrets

--- storage_workflows/crdb/commands/health_check.py
+++ storage_workflows/crdb/commands/health_check.py
@@ -675,6 +675,10 @@ def debug_secrets(deployment_env, cluster_name, region):
         secret_filters['tag-value'][-1] = cluster_name_with_hyphens
         secret_list = Secret.find_all_secrets(transform_filters(secret_filters))
 
+    for secret in secret_list:
+        secret_value = secret.get_secret_value()
+        logger.info(f"Secret Name: {secret.name}, Description: {secret.description}, Value: {secret_value}")
+
     # If secret_list is still empty after the retry, raise an error
     if not secret_list:
         raise ValueError(
