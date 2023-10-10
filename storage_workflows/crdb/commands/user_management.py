701bc0e46df054c6d9c67e3100d8d3f787c4eac4
meha-kaushik <meha.kaushik@doordash.com>
2023-10-09 21:13:45 -0700

    changes to reflect differences in service account bucket and readonly users bucket

--- storage_workflows/crdb/commands/user_management.py
+++ storage_workflows/crdb/commands/user_management.py
@@ -16,26 +16,40 @@ from storage_workflows.setup_env import setup_env
 app = typer.Typer()
 logger = Logger()
 
+
 @app.command()
-def create_users_from_s3_objects(deployment_env, region, bucket_name, aws_account, password=None):
+def create_users_from_s3_objects(deployment_env, region, bucket_name, aws_account, is_service_account:bool=True):
     setup_env(deployment_env, region, "")
     objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name)
     #Process the objects in the current page
     for obj in objects:
-        content = S3Gateway.read_object_contents(bucket_name=bucket_name, key=obj['Key'])
-        user_type, cluster_name, user_name = content.split(':')
-        logger.info("Read user_type : {}, cluster_name : {}, user_name : {}".format(user_type, cluster_name, user_name))
-        os.environ['CLUSTER_NAME'] = cluster_name
-        create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, cluster_name, user_name, user_type, password)
+        read_s3_object_contents_and_create_user(obj,is_service_account)
 
     while next_page_token:
         objects, next_page_token = S3Gateway.read_objects_with_pagination(bucket_name, page_token=next_page_token)
         for obj in objects:
-            content = S3Gateway.read_object_contents(bucket_name=bucket_name, key=obj['Key'])
-            user_type, cluster_name, user_name = content.split(':')
-            logger.info("Read user_type : {0}, cluster_name : {1}, user_name : {2}".format(user_type, cluster_name, user_name))
-            os.environ['CLUSTER_NAME'] = cluster_name
-            create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, cluster_name, user_name, user_type, password)
+            read_s3_object_contents_and_create_user(obj,is_service_account)
+
+
+def read_s3_object_contents_and_create_user(obj, is_service_account:bool=True):
+    s3_obj_key_name = obj['Key']
+    if is_service_account:
+        cluster_name = s3_obj_key_name
+        os.environ['CLUSTER_NAME'] = cluster_name
+        content = S3Gateway.read_object_contents(bucket_name=bucket_name, key=s3_obj_key_name)
+        s3_obj_lines = content.split('\n')
+        for line in s3_obj_lines:
+            user_type, db_name, user_name = line.split(':')
+            logger.info("Read user_type : {}, db_name : {}, user_name : {}".format(user_type, db_name, user_name))
+            create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, db_name, user_name, user_type,
+                                     password)
+    else:
+        cluster_name, db_name, user_name = s3_obj_key_name.split('_')  # db_name and user_name not being used from here
+        content = S3Gateway.read_object_contents(bucket_name=bucket_name, key=s3_obj_key_name)
+        user_type, db_name, user_name = content.split(':')
+        logger.info("Read user_type : {}, db_name : {}, user_name : {}".format(user_type, db_name, user_name))
+        os.environ['CLUSTER_NAME'] = cluster_name
+        create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, db_name, user_name, user_type, password)
 
 
 def create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, db_name, user_name, user_type, password=None):
@@ -44,17 +58,17 @@ def create_user_if_not_exist(cluster_name, deployment_env, region, aws_account, 
     if len(existing_users) == 0:
         match user_type:
             case "readonly":
-                user = ReadOnlyUser(user_name, cluster_name=cluster_name, db_name=cluster_name)
+                user = ReadOnlyUser(user_name, cluster_name=cluster_name, db_name=db_name)
             case "dba":
-                user = DbaUser(user_name, cluster_name=cluster_name, db_name=cluster_name)
+                user = DbaUser(user_name, cluster_name=cluster_name, db_name=db_name)
             case "app":
-                user = AppUser(user_name, cluster_name=cluster_name, db_name=cluster_name)
+                user = AppUser(user_name, cluster_name=cluster_name, db_name=db_name)
             case "ui":
-                user = DoorDashUser(user_name, cluster_name=cluster_name, db_name=cluster_name)
+                user = DoorDashUser(user_name, cluster_name=cluster_name, db_name=db_name)
             case "mode":
-                user = ModeUser(user_name, cluster_name=cluster_name, db_name=cluster_name, password=password)
+                user = ModeUser(user_name, cluster_name=cluster_name, db_name=db_name, password=password)
             case "analytics_exporter":
-                user = AnalyticUser(user_name, cluster_name=cluster_name, db_name=cluster_name, password=password)
+                user = AnalyticUser(user_name, cluster_name=cluster_name, db_name=db_name, password=password)
         user.create_user()
         storage_metadata.insert_user(cluster_name=cluster_name, deployment_env=deployment_env, region=region,
                                      aws_account=aws_account, database_name=db_name, role_name=user_name,
