f5ec975b9e7930ea287bc74f77b0dab91e3f1b6c
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-13 14:22:11 -0500

    refresh aws tokens

--- storage_workflows/crdb/factory/aws_session_factory.py
+++ storage_workflows/crdb/factory/aws_session_factory.py
@@ -1,87 +1,75 @@
 import boto3
 import os
-from functools import cache
+from botocore.exceptions import ClientError
 
 
 class AwsSessionFactory:
-    
+
     @staticmethod
-    @cache
+    def create_client(service_name):
+        try:
+            client = boto3.client(service_name,
+                                  aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                                  aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+                                  region_name=os.getenv('REGION'))
+            # Just a dummy call to check token validity.
+            if service_name == 's3':
+                client.list_buckets()
+            return client
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ExpiredToken':
+                AwsSessionFactory.refresh_token()
+                return AwsSessionFactory.create_client(service_name)
+            else:
+                raise e
+
+    @staticmethod
+    def refresh_token():
+        # Assuming you have a mechanism to refresh your token and set environment variables.
+        # Here's a simple example using sts. Adjust according to your setup.
+        sts_client = boto3.client('sts',
+                                  aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                                  region_name=os.getenv('REGION'))
+
+        response = sts_client.get_session_token()
+        os.environ['AWS_ACCESS_KEY_ID'] = response['Credentials']['AccessKeyId']
+        os.environ['AWS_SECRET_ACCESS_KEY'] = response['Credentials']['SecretAccessKey']
+        os.environ['AWS_SESSION_TOKEN'] = response['Credentials']['SessionToken']
+
+    @staticmethod
     def auto_scaling():
-        return boto3.client('autoscaling',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
-    
+        return AwsSessionFactory.create_client('autoscaling')
+
     @staticmethod
-    @cache
     def secret_manager():
-        return boto3.client("secretsmanager",
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
-    
+        return AwsSessionFactory.create_client('secretsmanager')
+
     @staticmethod
-    @cache
     def sts():
-        return boto3.client('sts',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
+        return AwsSessionFactory.create_client('sts')
 
     @staticmethod
-    @cache
     def ec2():
-        return boto3.client('ec2',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
-    
+        return AwsSessionFactory.create_client('ec2')
+
     @staticmethod
-    @cache
     def elb():
-        return boto3.client('elb',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
+        return AwsSessionFactory.create_client('elb')
 
     @staticmethod
-    @cache
     def iam():
-        return boto3.client('iam',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
+        return AwsSessionFactory.create_client('iam')
 
     @staticmethod
-    @cache
     def ebs():
-        return boto3.client('ebs',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
+        return AwsSessionFactory.create_client('ebs')
 
     @staticmethod
-    @cache
     def s3():
-        return boto3.client('s3',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
+        return AwsSessionFactory.create_client('s3')
 
     @staticmethod
-    @cache
     def elbv2():
-        return boto3.client('elbv2',
-                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
-                            region_name=os.getenv('REGION'))
+        return AwsSessionFactory.create_client('elbv2')
