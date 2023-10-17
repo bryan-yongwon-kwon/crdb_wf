2490356744e1ae2b9d2418391be2a93dcb4830ae
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-17 09:43:55 -0500

    disable auto_commit again

--- storage_workflows/crdb/connect/crdb_connection.py
+++ storage_workflows/crdb/connect/crdb_connection.py
@@ -123,7 +123,7 @@ class CrdbConnection:
             self._connection.close()
 
     def execute_sql(self, sql: str, need_commit: bool = False, need_fetchall: bool = True, need_fetchone: bool = False,
-                    need_connection_close: bool = False, auto_commit: bool = True):
+                    need_connection_close: bool = False, auto_commit: bool = False):
         try:
             if self._connection is None:
                 raise ValueError("Connection is not established.")
