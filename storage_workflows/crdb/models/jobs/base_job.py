649de076585420491abb2362c7b4627808f39859
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-31 12:27:24 -0500

    update base job

--- storage_workflows/crdb/models/jobs/base_job.py
+++ storage_workflows/crdb/models/jobs/base_job.py
@@ -11,16 +11,13 @@ class BaseJob:
     PAUSE_JOB_BY_ID_SQL = "PAUSE JOB {};"
     RESUME_JOB_BY_ID_SQL = "RESUME JOB {};"
     GET_JOB_BY_ID_SQL = "SELECT job_id, job_type, status FROM crdb_internal.jobs AS OF SYSTEM TIME FOLLOWER_READ_TIMESTAMP() WHERE job_id='{}';"
-    GET_CHANGEFEED_METADATA = ("SELECT running_status, error, (((high_water_timestamp/1e9)::INT)-NOW()::INT) AS "
-                               "latency, CASE WHEN description like '%initial_scan = ''only''%' then TRUE ELSE FALSE "
-                               "END AS is_initial_scan_only, (finished::INT-now()::INT) as finished_ago_seconds, "
-                               "description, high_water_timestamp FROM crdb_internal.jobs AS OF SYSTEM TIME "
-                               "FOLLOWER_READ_TIMESTAMP() WHERE job_type = 'CHANGEFEED' AND job_id = '{}';")
 
-    def __init__(self, job_id, job_type, status, cluster_name):
+    def __init__(self, job_id, job_type, status, description, high_water_timestamp, cluster_name):
         self._job_id = job_id
         self._job_type = job_type
         self._status = status
+        self._description = description
+        self._high_water_timestamp = high_water_timestamp
         self._cluster_name = cluster_name
 
     @property
@@ -39,20 +36,13 @@ class BaseJob:
     def status(self):
         return self._status
 
-    @cached_property
-    def changefeed_metadata(self):
-        result = self.connection.execute_sql(self.GET_CHANGEFEED_METADATA.format(self.id))
-        return result[0] if result else None
-
     @property
     def description(self):
-        metadata = self.changefeed_metadata
-        return metadata[5] if metadata else None
+        return self._description
 
     @property
     def high_water_timestamp(self):
-        metadata = self.changefeed_metadata
-        return metadata[6] if metadata else None
+        return self._high_water_timestamp
 
     @cached_property
     def connection(self):
