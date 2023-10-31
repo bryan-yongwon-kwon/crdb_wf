ee448f8d2d22d4538dd5328bdf0f42c9c35f347b
Bryan Kwon <bryan@cockroachlabs.com>
2023-10-31 11:51:57 -0500

    persist changefeed details

--- storage_workflows/crdb/models/jobs/base_job.py
+++ storage_workflows/crdb/models/jobs/base_job.py
@@ -1,19 +1,28 @@
 from functools import cached_property
 from storage_workflows.crdb.connect.crdb_connection import CrdbConnection
 
-class BaseJob:
 
-    FIND_ALL_JOBS_BY_TYPE_SQL = "SELECT job_id,job_type,status from crdb_internal.jobs AS OF SYSTEM TIME FOLLOWER_READ_TIMESTAMP() WHERE job_type = '{}' AND ((job_type IS NULL) OR ((job_type NOT IN ('AUTO CREATE STATS', 'AUTO SCHEMA TELEMETRY', 'AUTO SPAN CONFIG RECONCILIATION', 'AUTO SQL STATS COMPACTION')) AND ((finished IS NULL) OR (finished > NOW() - INTERVAL '12h' ))));"
+class BaseJob:
+    FIND_ALL_JOBS_BY_TYPE_SQL = ("SELECT job_id, job_type, status "
+                                 "from crdb_internal.jobs AS OF SYSTEM TIME FOLLOWER_READ_TIMESTAMP() "
+                                 "WHERE job_type = '{}' AND ((job_type IS NULL) OR ((job_type NOT IN ('AUTO CREATE STATS', "
+                                 "'AUTO SCHEMA TELEMETRY', 'AUTO SPAN CONFIG RECONCILIATION', 'AUTO SQL STATS COMPACTION')) "
+                                 "AND ((finished IS NULL) OR (finished > NOW() - INTERVAL '12h' ))));")
     PAUSE_JOB_BY_ID_SQL = "PAUSE JOB {};"
     RESUME_JOB_BY_ID_SQL = "RESUME JOB {};"
-    GET_JOB_BY_ID_SQL = "SELECT job_id,job_type,status FROM crdb_internal.jobs AS OF SYSTEM TIME FOLLOWER_READ_TIMESTAMP() WHERE job_id='{}';"
+    GET_JOB_BY_ID_SQL = "SELECT job_id, job_type, status FROM crdb_internal.jobs AS OF SYSTEM TIME FOLLOWER_READ_TIMESTAMP() WHERE job_id='{}';"
+    GET_CHANGEFEED_METADATA = ("SELECT running_status, error, (((high_water_timestamp/1e9)::INT)-NOW()::INT) AS "
+                               "latency, CASE WHEN description like '%initial_scan = ''only''%' then TRUE ELSE FALSE "
+                               "END AS is_initial_scan_only, (finished::INT-now()::INT) as finished_ago_seconds, "
+                               "description, high_water_timestamp FROM crdb_internal.jobs AS OF SYSTEM TIME "
+                               "FOLLOWER_READ_TIMESTAMP() WHERE job_type = 'CHANGEFEED' AND job_id = '{}';")
 
     def __init__(self, job_id, job_type, status, cluster_name):
         self._job_id = job_id
         self._job_type = job_type
         self._status = status
         self._cluster_name = cluster_name
-    
+
     @property
     def cluster_name(self):
         return self._cluster_name
@@ -21,14 +30,29 @@ class BaseJob:
     @property
     def id(self):
         return self._job_id
-    
+
     @property
     def type(self):
         return self._job_type
-    
+
     @property
     def status(self):
         return self._status
+
+    @cached_property
+    def changefeed_metadata(self):
+        result = self.connection.execute_sql(self.GET_CHANGEFEED_METADATA.format(self.id))
+        return result[0] if result else None
+
+    @property
+    def description(self):
+        metadata = self.changefeed_metadata
+        return metadata[5] if metadata else None
+
+    @property
+    def high_water_timestamp(self):
+        metadata = self.changefeed_metadata
+        return metadata[6] if metadata else None
 
     @cached_property
     def connection(self):
--- storage_workflows/metadata_db/crdb_workflows/transactions.py
+++ storage_workflows/metadata_db/crdb_workflows/transactions.py
@@ -3,8 +3,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import insert
 from storage_workflows.metadata_db.crdb_workflows.tables.clusters_info import ClustersInfo
 from storage_workflows.logging.logger import Logger
+from storage_workflows.metadata_db.crdb_workflows.tables.changefeed_job_details import ChangefeedJobDetails
 
 logger = Logger()
+
 
 def get_instance_ids_txn(session: Session, cluster_name:str, deployment_env:str):
     statement = select(ClustersInfo.instance_ids).where(ClustersInfo.cluster_name == cluster_name,
@@ -15,6 +17,7 @@ def get_instance_ids_txn(session: Session, cluster_name:str, deployment_env:str)
     
     return crdb_cluster.instance_ids
 
+
 def upsert_instancer_ids_txn(session: Session, cluster_name:str, deployment_env:str, instance_ids:list[str]):
     insert_statement = insert(ClustersInfo).values(cluster_name=cluster_name,
                                                    deployment_env=deployment_env,
@@ -22,3 +25,23 @@ def upsert_instancer_ids_txn(session: Session, cluster_name:str, deployment_env:
     upsert_statement = insert_statement.on_conflict_do_update(constraint='clusters_info_pkey',
                                                               set_=dict(instance_ids=instance_ids))
     session.execute(upsert_statement)
+
+
+def _insert_changefeed_job_details_txn(session, job_detail):
+    """Insert a new ChangefeedJobDetails record transaction function."""
+    session.add(job_detail)
+
+
+def _get_changefeed_job_details_txn(session, workflow_id):
+    """Retrieve ChangefeedJobDetails record by workflow_id transaction function."""
+    return session.query(ChangefeedJobDetails).filter(ChangefeedJobDetails.workflow_id == workflow_id).one_or_none()
+
+
+def _update_changefeed_job_details_txn(session, workflow_id, updated_data):
+    """Update ChangefeedJobDetails record by workflow_id transaction function."""
+    session.query(ChangefeedJobDetails).filter(ChangefeedJobDetails.workflow_id == workflow_id).update(updated_data)
+
+
+def _delete_changefeed_job_details_txn(session, workflow_id):
+    """Delete ChangefeedJobDetails record by workflow_id transaction function."""
+    session.query(ChangefeedJobDetails).filter(ChangefeedJobDetails.workflow_id == workflow_id).delete()
