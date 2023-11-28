a1f143a9100b27e2cad2a73da6ecb01e8194721a
Bryan Kwon <bryan@cockroachlabs.com>
2023-11-28 10:13:46 -0600

    add schema for ipu workflow

---
+++ storage_workflows/crdb/commands/ipu.py
@@ -0,0 +1,84 @@
+import datetime
+import json
+import math
+import os
+import statistics
+import sys
+import time
+import typer
+from storage_workflows.chronosphere.chronosphere_api_gateway import ChronosphereApiGateway
+from storage_workflows.crdb.api_gateway.elastic_load_balancer_gateway import ElasticLoadBalancerGateway
+from storage_workflows.crdb.api_gateway.auto_scaling_group_gateway import AutoScalingGroupGateway
+from storage_workflows.crdb.aws.auto_scaling_group import AutoScalingGroup
+from storage_workflows.crdb.aws.elastic_load_balancer import ElasticLoadBalancer
+from storage_workflows.crdb.aws.ec2_instance import Ec2Instance
+from storage_workflows.crdb.connect.ssh import SSH
+from storage_workflows.crdb.metadata_db.metadata_db_operations import MetadataDBOperations
+from storage_workflows.crdb.models.cluster import Cluster
+from storage_workflows.crdb.models.node import Node
+from storage_workflows.crdb.models.jobs.changefeed_job import ChangefeedJob
+from storage_workflows.crdb.slack.content_templates import ContentTemplate
+from storage_workflows.global_change_log.global_change_log_gateway import GlobalChangeLogGateway
+from storage_workflows.global_change_log.service_name import ServiceName
+from storage_workflows.logging.logger import Logger
+from storage_workflows.setup_env import setup_env
+from storage_workflows.slack.slack_notification import SlackNotification
+
+app = typer.Typer()
+logger = Logger()
+
+
+
+@app.command()
+def run_debug_doctor(deployment_env, region, cluster_name):
+    """
+    Checks for table descriptor corruption in preparation for major version upgrade
+    """
+    setup_env(deployment_env, region, cluster_name)
+    nodes = Node.get_nodes()
+    if nodes:
+        nodes[0].check_table_descriptor_corruption()
+    else:
+        logger.error("No nodes found in the cluster.")
+
+
+@app.command()
+def pre_check(deployment_env, region, cluster_name):
+    setup_env(deployment_env, region, cluster_name)
+    # handle unusual cluster names with dashes: e.g. url-shortener
+    cluster_name = os.environ['CLUSTER_NAME']
+    workflow_id = os.getenv('WORKFLOW-ID')
+    cluster = Cluster()
+    logger.info(f"workflow_id: {workflow_id}")
+    if (cluster.backup_job_is_running()
+        or cluster.restore_job_is_running()
+        or cluster.schema_change_job_is_running()
+        or cluster.row_level_ttl_job_is_running()
+        or cluster.instances_not_in_service_exist()
+        or cluster.paused_changefeed_jobs_exist()
+        or cluster.unhealthy_ranges_exist()):
+        raise Exception("Pre run check failed")
+    else:
+        logger.info(f"{cluster_name} Check passed")
+        ChangefeedJob.persist_to_metadata_db(workflow_id, cluster_name)
+
+@app.command()
+def detach_instances_from_asg(deployment_env, region, cluster_name, timeout_minus):
+    setup_env(deployment_env, region, cluster_name)
+    # handle unusual cluster names with dashes: e.g. url-shortener
+    cluster_name = os.environ['CLUSTER_NAME']
+    asg = AutoScalingGroup.find_auto_scaling_group_by_cluster_name(cluster_name)
+    logger.info(f"{cluster_name} Autoscaling group name is {asg.name}")
+    # get instance ids of old nodes
+    metadata_db_operations = MetadataDBOperations()
+    old_instance_ids = metadata_db_operations.get_old_instance_ids(cluster_name, deployment_env)
+    # STORAGE-7583: do nothing if scaling up
+    if old_instance_ids:
+        for index in range(0, len(old_instance_ids), 12):
+          AutoScalingGroupGateway.detach_instance_from_autoscaling_group(old_instance_ids[index:index+12], asg.name)
+        cluster = Cluster()
+        cluster.wait_for_connections_drain_on_old_nodes(int(timeout_minus))
+        logger.info(f"{cluster_name} detached instances from asg")
+        return
+    else:
+        logger.info(f"{cluster_name} no instances found. skipping detach instances from asg. ")
---
+++ storage_workflows/metadata_db/crdb_workflows/tables/crdb_dbops_wf.py
@@ -0,0 +1,55 @@
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
+from sqlalchemy import Column, String, DateTime, Enum
+from sqlalchemy.orm import declarative_base, mapped_column, Mapped
+from sqlalchemy.sql import func
+import enum
+import uuid
+import datetime
+
+Base = declarative_base()
+
+class WorkflowStatus(enum.Enum):
+    NEW = "new"
+    RUNNING = "running"
+    PAUSED = "paused"
+    COMPLETED = "completed"
+
+class CRDBDbOpsWFEntry(Base):
+    """The CRDBDbOpsWFEntry class corresponds to the "crdb_dbops_wf_entry" database table."""
+    __tablename__ = "crdb_dbops_wf_entry"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    cluster_name: Mapped[str] = mapped_column(String)
+    region: Mapped[str] = mapped_column(String)
+    deployment_env: Mapped[str] = mapped_column(String)
+    operation_type: Mapped[str] = mapped_column(String)
+    operator_name: Mapped[str] = mapped_column(String)
+    status: Mapped[WorkflowStatus] = mapped_column(Enum(WorkflowStatus))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self) -> str:
+        return f"CRDBDbOpsWFEntry(id={self.id!r}, cluster_name={self.cluster_name!r}, region={self.region!r}, \
+                deployment_env={self.deployment_env!r}, operation_type={self.operation_type!r}, \
+                operator_name={self.operator_name!r}, status={self.status!r}, \
+                created_at={self.created_at!r}, updated_at={self.updated_at!r})"
+
+class CRDBDbOpsWFEvent(Base):
+    """The CRDBDbOpsWFEvent class corresponds to the "crdb_dbops_wf_event" database table."""
+    __tablename__ = "crdb_dbops_wf_event"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    workflow_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True))  # This should be a foreign key to CRDBDbOpsWFEntry.id
+    cluster_name: Mapped[str] = mapped_column(String)
+    region: Mapped[str] = mapped_column(String)
+    deployment_env: Mapped[str] = mapped_column(String)
+    operation_type: Mapped[str] = mapped_column(String)
+    operator_name: Mapped[str] = mapped_column(String)
+    event_detail: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self) -> str:
+        return f"CRDBDbOpsWFEvent(id={self.id!r}, workflow_id={self.workflow_id!r}, cluster_name={self.cluster_name!r}, \
+                region={self.region!r}, deployment_env={self.deployment_env!r}, operation_type={self.operation_type!r}, \
+                operator_name={self.operator_name!r}, event_detail={self.event_detail!r}, \
+                created_at={self.created_at!r})"
